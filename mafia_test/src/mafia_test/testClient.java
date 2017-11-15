c35cbac3d6d4bfec1d7151079d154708188715ae
신한섭 <@HANSEOP>
2017-11-15 20:42:24 +0900

    test

--- mafia_test/src/mafia_test/testClient.java
+++ mafia_test/src/mafia_test/testClient.java
@@ -30,90 +30,92 @@ import javax.swing.JTextField;
  */
 public class testClient {
 
-   BufferedReader in;
-   PrintWriter out;
-   JFrame frame = new JFrame("Chatter");
-   JTextField textField = new JTextField(40);
-   JTextArea messageArea = new JTextArea(40, 40);
+	BufferedReader in;
+	PrintWriter out;
+	JFrame frame = new JFrame("Chatter");
+	JTextField textField = new JTextField(40);
+	JTextArea messageArea = new JTextArea(40, 40);
 
-   /**
-    * Constructs the client by laying out the GUI and registering a listener with
-    * the textfield so that pressing Return in the listener sends the textfield
-    * contents to the server. Note howe127.0.0.1ver that the textfield is initially
-    * NOT editable, and only becomes editable AFTER the client receives the
-    * NAMEACCEPTED message from the server.
-    */
-   public testClient() {
+	/**
+	 * Constructs the client by laying out the GUI and registering a listener with
+	 * the textfield so that pressing Return in the listener sends the textfield
+	 * contents to the server. Note howe127.0.0.1ver that the textfield is initially
+	 * NOT editable, and only becomes editable AFTER the client receives the
+	 * NAMEACCEPTED message from the server.
+	 */
+	public testClient() {
 
-      // Layout GUI
-      textField.setEditable(false);
-      messageArea.setEditable(false);
-      frame.getContentPane().add(textField, "North");
-      frame.getContentPane().add(new JScrollPane(messageArea), "Center");
-      frame.pack();
+		// Layout GUI
+		textField.setEditable(false);
+		messageArea.setEditable(false);
+		frame.getContentPane().add(textField, "North");
+		frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+		frame.pack();
 
-      // Add Listeners
-      textField.addActionListener(new ActionListener() {
-         /**
-          * Responds to pressing the enter key in the textfield by sending the contents
-          * of the text field to the server. Then clear the text area in preparation for
-          * the next message.
-          */
-         public void actionPerformed(ActionEvent e) {
-            out.println(textField.getText());// 메세지 보내는 부분
-            textField.setText("");// 버퍼 비워줌
-         }
-      });
-   }
+		// Add Listeners
+		textField.addActionListener(new ActionListener() {
+			/**
+			 * Responds to pressing the enter key in the textfield by sending the contents
+			 * of the text field to the server. Then clear the text area in preparation for
+			 * the next message.
+			 */
+			public void actionPerformed(ActionEvent e) {
+				out.println(textField.getText());// 메세지 보내는 부분
+				textField.setText("");// 버퍼 비워줌
+			}
+		});
+	}
 
-   /**
-    * Prompt for and return the address of the server.
-    */
-   private String getServerAddress() {
-      return JOptionPane.showInputDialog(frame, "Enter IP Address of the Server:", "Welcome to the Chatter",
-            JOptionPane.QUESTION_MESSAGE);
-   }
+	/**
+	 * Prompt for and return the address of the server.
+	 */
+	private String getServerAddress() {
+		return JOptionPane.showInputDialog(frame, "Enter IP Address of the Server:", "Welcome to the Chatter",
+				JOptionPane.QUESTION_MESSAGE);
+	}
 
-   /**
-    * Prompt for and return the desired screen name.
-    */
-   private String getName() {
-      return JOptionPane.showInputDialog(frame, "Choose a screen name:", "Screen name selection",
-            JOptionPane.PLAIN_MESSAGE);
-   }
+	/**
+	 * Prompt for and return the desired screen name.
+	 */
+	private String getName() {
+		return JOptionPane.showInputDialog(frame, "Choose a screen name:", "Screen name selection",
+				JOptionPane.PLAIN_MESSAGE);
+	}
 
-   /**
-    * Connects to the server then enters the processing loop.
-    */
-   private void run() throws IOException {
+	/**
+	 * Connects to the server then enters the processing loop.
+	 */
+	private void run() throws IOException {
 
-      // Make connection and initialize streams
-      String serverAddress = getServerAddress(); // serveraddress : ip address
-      Socket socket = new Socket(serverAddress, 9001);
-      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      out = new PrintWriter(socket.getOutputStream(), true);
+		// Make connection and initialize streams
+		String serverAddress = getServerAddress(); // serveraddress : ip address
+		Socket socket = new Socket(serverAddress, 9001);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintWriter(socket.getOutputStream(), true);
 
-      // Process all messages from server, according to the protocol.
-      while (true) {
-         String line = in.readLine();
-         if (line.startsWith("SUBMITNAME")) {
-            out.println(getName()); // input name
-         } else if (line.startsWith("NAMEACCEPTED")) {
-            textField.setEditable(true);
-         } else if (line.startsWith("MESSAGE")) {
-            messageArea.append(line.substring(8) + "\n");// input message
-         } else if (line.startsWith("KICKED"))
-            textField.setEditable(false);
-      }
-   }
+		// Process all messages from server, according to the protocol.
+		while (true) {
+			String line = in.readLine();
+			if (line.startsWith("SUBMITNAME")) {
+				out.println(getName()); // input name
+			} else if (line.startsWith("NAMEACCEPTED")) {
+				textField.setEditable(true);
+			} else if (line.startsWith("MESSAGE")) {
+				messageArea.append(line.substring(8) + "\n");// input message
+			} else if (line.startsWith("KICKED")) {
+				textField.setEditable(false);
+				break;
+			}
+		}
+	}
 
-   /**
-    * Runs the client as an application with a closeable frame.
-    */
-   public static void main(String[] args) throws Exception {
-      testClient client = new testClient();
-      client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      client.frame.setVisible(true);
-      client.run();
-   }
+	/**
+	 * Runs the client as an application with a closeable frame.
+	 */
+	public static void main(String[] args) throws Exception {
+		testClient client = new testClient();
+		client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.frame.setVisible(true);
+		client.run();
+	}
 }
