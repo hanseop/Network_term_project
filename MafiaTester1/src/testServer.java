c06072e237321ae14ac93e59c1c9de6d0c63a0f0
hanseop <32629025+hanseop@users.noreply.github.com>
2017-11-27 04:00:02 +0900

    add mafia function and police function

--- MafiaTester1/src/testServer.java
+++ MafiaTester1/src/testServer.java
@@ -66,7 +66,8 @@ public class testServer {
 	private static void initialize(int[] vote, int[] kicked, String[] user, PrintWriter[] ID) {
 		for (int i = 0; i < max_client; i++) {
 			/**************************** modified *******************************/
-			kicked[i] = 1;/**************************************************************/
+			kicked[i] = 1;
+			/**************************************************************/
 			vote[i] = 0;
 			user[i] = "null";
 			ID[i] = null;
@@ -100,163 +101,6 @@ public class testServer {
 				police_index = i;
 			else if ((job[i].substring(0, job[i].indexOf(" "))).equals("doctor"))
 				doctor_index = i;
-		}
-	}
-
-	public static void whisper(String input, String name) {
-		String whisper;
-		whisper = input.substring(1, input.indexOf("/>"));
-
-		if (names.contains(whisper)) {
-			PrintWriter sender = info.get(name);
-			PrintWriter receiver = info.get(whisper);
-			receiver.println("MESSAGE " + "<whisper from " + name + "> : " + input.substring(whisper.length() + 3));
-			sender.println("MESSAGE " + "<whisper to " + whisper + "> : " + input.substring(whisper.length() + 3));
-		} else {
-			PrintWriter sender = info.get(name);
-			sender.println("MESSAGE " + "This user does not exist.");
-		}
-
-	}
-
-	public static void whatIsMyJob(String name) {
-		int temp_index = 0;
-
-		for (int i = 0; i < client_count; i++) {
-			if (name == user[i])
-				temp_index = i;
-		}
-
-		PrintWriter sender = info.get(name);
-		sender.println("MESSAGE " + "your job is " + job[temp_index].substring(0, job[temp_index].indexOf(" ")));
-	}
-
-	public static void whatIsMyRole(String name) {
-		int temp_index = 0;
-		for (int i = 0; i < client_count; i++) {
-			if (name == user[i])
-				temp_index = i;
-		}
-		PrintWriter sender = info.get(name);
-		sender.println("MESSAGE " + "your role is " + job[temp_index].substring(job[temp_index].indexOf(" ")));
-	}
-
-	public static void vote() {
-		String temp = null;
-		for (int i = 0; i < max_client; i++) {
-			if (kicked[i] != 0) {
-				if (temp == null) {
-					temp = user[i];
-				} else {
-					temp += ("," + user[i]);
-				}
-			}
-		}
-		System.out.println(temp);
-		for (PrintWriter writer : writers) {
-			writer.println("VOTENAME " + temp);
-		}
-	}
-
-	public static void whoIsVictim(String input) {
-		String victim = input.substring(7);
-		int temp_index = 0;
-		for (int i = 0; i < max_client; i++) {
-			if (user[i].equals(victim) && kicked[i] != 0)
-				temp_index = i;
-		}
-		is_vote++;
-		vote[temp_index]++;
-	}
-
-	public static void showJob(String name) {
-		PrintWriter police = info.get(name);
-		String temp = null;
-		for (int i = 0; i < max_client; i++) {
-			if (kicked[i] != 0 && !user[i].equals(user[police_index])) {
-				if (temp == null) {
-					temp = user[i];
-				} else {
-					temp += ("," + user[i]);
-				}
-			}
-		}
-		System.out.println(temp);
-		if (name.equals(user[police_index])) {
-			police.println("JOB" + temp);
-		} else {
-			police.println("MESSAGE " + "You are not police");
-		}
-	}
-
-	public static void isHeMafia(String name, String input) {
-		PrintWriter police = info.get(name);
-		String selected = input.substring(8);
-		int temp_index = 0;
-		for (int i = 0; i < max_client; i++) {
-			if (user[i].equals(selected) && kicked[i] != 0)
-				temp_index = i;
-		}
-		police.println("IS_MAFIA?" + user[temp_index] + "' job is"
-				+ job[temp_index].substring(job[temp_index].indexOf(" ") + 1));
-	}
-
-	public static void broadcast(String name, String input) {
-		for (PrintWriter writer : writers) {
-			writer.println("MESSAGE " + name + ": " + input);
-		}
-	}
-
-	public static void voteResult() {
-		if (is_vote == client_count) {
-			int count = 0;
-			int temp_index = 0;
-			int same = 0;
-			for (int i = 0; i < max_client; i++) {
-				if (vote[i] > count) {
-					count = vote[i];
-					temp_index = i;
-				}
-			}
-			for (int i = 0; i < max_client; i++) {
-				if (count == vote[i] && i != temp_index)
-					same = 1;
-			}
-
-			if (count == 0)
-				same = 0;
-
-			if (same != 1) {
-				for (PrintWriter writer : writers) {
-					writer.println("MESSAGE " + user[temp_index] + " kicked");
-				}
-				PrintWriter victim = info.get(user[temp_index]);
-				victim.println("KICKED");
-				kicked[temp_index] = 0;
-				current_client--;
-			} else {
-				for (PrintWriter writer : writers) {
-					writer.println("MESSAGE " + "Nothing happened");
-				}
-			}
-			is_vote = 0;
-			count = 0;
-			for (int i = 0; i < max_client; i++)
-				vote[i] = 0;
-		}
-	}
-
-	public static void whoIsWin() {
-		if (kicked[mafia_index] == 0) {
-			for (PrintWriter writer : writers) {
-				writer.println("MESSAGE " + "mafia dead!, citizen win!");
-			}
-			System.exit(0);
-		} else if (current_client == 2) {
-			for (PrintWriter writer : writers) {
-				writer.println("MESSAGE " + "mafia win!");
-			}
-			System.exit(0);
 		}
 	}
 
@@ -317,8 +161,8 @@ public class testServer {
 							names.add(name);
 
 							for (PrintWriter writer : writers) { // if any user enters, memo in server's console.
-																	// 맞나..??? -> 새로운 유저가 들어올 경우 채팅창으로 유저들에게 [name]
-																	// enter라고 알려줌
+								// 맞나..??? -> 새로운 유저가 들어올 경우 채팅창으로 유저들에게 [name]
+								// enter라고 알려줌
 								writer.println("MESSAGE " + "[" + name + "] enter");
 							}
 							break;
@@ -326,7 +170,7 @@ public class testServer {
 					}
 				}
 				out.println("NAMEACCEPTED"); // if user enters own name, give check-message. ??? ->클라이언트에게
-												// NAMEACCPETED라는 프로토콜메세지를 보내줌 ->유저의 채팅창 활성화 시켜줌
+				// NAMEACCPETED라는 프로토콜메세지를 보내줌 ->유저의 채팅창 활성화 시켜줌
 				writers.add(out); // ??? -> 유저의 주소를 hashset에 저장
 				user[client_count] = name; // store a name in array.
 				ID[client_count] = out; // store an ID in array.
@@ -351,7 +195,14 @@ public class testServer {
 						job[random[i]] = temp;
 					}
 
-					storeIndex();
+					for (int i = 0; i < max_client; i++) {
+						if ((job[i].substring(0, job[i].indexOf(" "))).equals("mafia"))
+							mafia_index = i;
+						else if ((job[i].substring(0, job[i].indexOf(" "))).equals("police"))
+							police_index = i;
+						else if ((job[i].substring(0, job[i].indexOf(" "))).equals("doctor"))
+							doctor_index = i;
+					}
 					// tell a job to each players.? 이 과정이 맞나..?--직업배정받은 정보를 소켓에 담아 클라이언트에게 주면 팝업창으로
 					// 알려주는 게 좋을 것 같음...
 					for (int i = 0; i < max_client; i++)
@@ -368,22 +219,58 @@ public class testServer {
 
 					// whisper..?????? 필요한 이유..??? -> PA할때 쓰던 코드를 그대로 써서 있음 삭제해도 무방
 					else if (input.startsWith("<") && input.indexOf("/>") != -1) {
-						whisper(input, name);
+						String whisper;
+						whisper = input.substring(1, input.indexOf("/>"));
+
+						if (names.contains(whisper)) {
+							PrintWriter sender = info.get(name);
+							PrintWriter receiver = info.get(whisper);
+							receiver.println("MESSAGE " + "<whisper from " + name + "> : "
+									+ input.substring(whisper.length() + 3));
+							sender.println("MESSAGE " + "<whisper to " + whisper + "> : "
+									+ input.substring(whisper.length() + 3));
+						} else {
+							PrintWriter sender = info.get(name);
+							sender.println("MESSAGE " + "This user does not exist.");
+						}
+
 					}
 					// tell a job --명령어를 써야만 직업을 알려주는..??것 보다는 일방적으로 알려주는게 좋을 것 같아요~
 					// 직업배정받은 정보를 소켓에 담아 클라이언트에게 주면 팝업창으로 알려주는 게 좋을 것 같음... !
 					// 현재 서버에 구현해놓은 기능은 gui와 연동되지 않고 순수 기능만 구현해 놓았습니다. 추후 gui가 완성되면 구현 예정
 					else if (input.startsWith("/") && input.indexOf("job") != -1) {
-						whatIsMyJob(input);
+						int temp_index = 0;
+
+						for (int i = 0; i < client_count; i++) {
+							if (name == user[i])
+								temp_index = i;
+						}
+
+						PrintWriter sender = info.get(name);
+						sender.println("MESSAGE " + "your job is "
+								+ job[temp_index].substring(0, job[temp_index].indexOf(" ")));
 					}
 					// job과 role의 차이??? job : 직업 role : 역할(능력)
 					else if (input.startsWith("/") && input.indexOf("role") != -1) {
-						whatIsMyRole(input);
+						int temp_index = 0;
+						for (int i = 0; i < client_count; i++) {
+							if (name == user[i])
+								temp_index = i;
+						}
+						PrintWriter sender = info.get(name);
+						sender.println(
+								"MESSAGE " + "your role is " + job[temp_index].substring(job[temp_index].indexOf(" ")));
 					}
 					// 투표는 5분타이머 이후에 패널-팝업창을 띄워서 바로 하게 만드는게 좋을 것 같아요.
 					// 명령어를 입력하고 투표하는 방식은, 클라이언트마다 각자 다른 시기에 선택할 것 같아서요~
 					// 현재 서버에 구현해놓은 기능은 gui와 연동되지 않고 순수 기능만 구현해 놓았습니다. 추후 gui가 완성되면 구현 예정
-
+					else if (input.startsWith("/") && input.indexOf("vote") != -1) {
+						int temp_index = 0;
+						for (int i = 0; i < client_count; i++) {
+							if (name == user[i])
+								temp_index = i;
+						}
+					}
 					// 이름??? 뭐하는 부분인지 잘 모르겠어요...
 					// 11월24일 모임때 말씀하셨던 투표를 할때 서버에서 클라이언트들의 이름을 받아오기 위한 과정입니다. 유저가 채팅창에 /name이라고 치면
 					// 현재 들어와 있는 유저들의 이름이 gui팝업창으로 뜸
@@ -392,22 +279,147 @@ public class testServer {
 					 * modified
 					 *******************************************/
 					else if (input.startsWith("/") && input.indexOf("name") != -1) {
-						vote();
+						String temp = null;
+						for (int i = 0; i < max_client; i++) {
+							if (kicked[i] != 0) {
+								if (temp == null) {
+									temp = user[i];
+								} else {
+									temp += ("," + user[i]);
+								}
+							}
+						}
+						System.out.println(temp);
+						for (PrintWriter writer : writers) {
+							writer.println("VOTENAME " + temp);
+						}
 					} else if (input.startsWith("/") && input.indexOf("victim") != -1) {
-						whoIsVictim(input);
+						String victim = input.substring(7);
+						int temp_index = 0;
+						for (int i = 0; i < max_client; i++) {
+							if (user[i].equals(victim) && kicked[i] != 0)
+								temp_index = i;
+						}
+						is_vote++;
+						vote[temp_index]++;
+					} else if (input.startsWith("/") && input.indexOf("kill") != -1) {
+						PrintWriter mafia = info.get(name);
+						String temp = null;
+						for (int i = 0; i < max_client; i++) {
+							if (kicked[i] != 0 && !user[i].equals(user[mafia_index])) {
+								if (temp == null) {
+									temp = user[i];
+								} else {
+									temp += ("," + user[i]);
+								}
+							}
+						}
+						System.out.println(temp);
+						if (name.equals(user[mafia_index])) {
+							mafia.println("KILL" + temp);
+						} else {
+							mafia.println("MESSAGE " + "You are not mafia");
+						}
+					} else if (input.startsWith("/") && input.indexOf("dead") != -1) {
+						PrintWriter mafia = info.get(name);
+						String selected = input.substring(5);
+						PrintWriter dead = info.get(selected);
+						int temp_index = 0;
+						for (int i = 0; i < max_client; i++) {
+							if (user[i].equals(selected) && kicked[i] != 0)
+								temp_index = i;
+						}
+						mafia.println("DEAD" + user[temp_index] + " dead");
+						dead.println("KICKED");
+						kicked[temp_index] = 0;
+						current_client--;
 					} else if (input.startsWith("/") && input.indexOf("police") != -1) {
-						showJob(name);
+						PrintWriter police = info.get(name);
+						String temp = null;
+						for (int i = 0; i < max_client; i++) {
+							if (kicked[i] != 0 && !user[i].equals(user[police_index])) {
+								if (temp == null) {
+									temp = user[i];
+								} else {
+									temp += ("," + user[i]);
+								}
+							}
+						}
+						System.out.println(temp);
+						if (name.equals(user[police_index])) {
+							police.println("JOB" + temp);
+						} else {
+							police.println("MESSAGE " + "You are not police");
+						}
 					} else if (input.startsWith("/") && input.indexOf("is_he_mafia?") != -1) {
-						isHeMafia(input, input);
+						PrintWriter police = info.get(name);
+						String selected = input.substring(13);
+						int temp_index = 9999;
+						System.out.println("selected : " + selected);
+						for (int i = 0; i < max_client; i++) {
+							if (user[i].equals(selected) && kicked[i] != 0)
+								temp_index = i;
+							System.out.println("user[" + i + "] : " + user[i]);
+						}
+						police.println("IS_MAFIA?" + user[temp_index] + "' job is "
+								+ job[temp_index].substring(0, job[temp_index].indexOf(" ")));
 					}
 					// 뭐하는 부분인지 잘 모르겠어요..ㅠㅠ
 					// 유저가 채팅을 치면 다른 유저들에게 보내주는 기능
 					else {
-						broadcast(input, input);
+						for (PrintWriter writer : writers) {
+							writer.println("MESSAGE " + name + ": " + input);
+						}
 					}
-					voteResult();
-					whoIsWin();
 
+					if (is_vote == client_count) {
+						int count = 0;
+						int temp_index = 0;
+						int same = 0;
+						for (int i = 0; i < max_client; i++) {
+							if (vote[i] > count) {
+								count = vote[i];
+								temp_index = i;
+							}
+						}
+						for (int i = 0; i < max_client; i++) {
+							if (count == vote[i] && i != temp_index)
+								same = 1;
+						}
+
+						if (count == 0)
+							same = 0;
+
+						if (same != 1) {
+							for (PrintWriter writer : writers) {
+								writer.println("MESSAGE " + user[temp_index] + " kicked");
+							}
+							PrintWriter victim = info.get(user[temp_index]);
+							victim.println("KICKED");
+							kicked[temp_index] = 0;
+							current_client--;
+						} else {
+							for (PrintWriter writer : writers) {
+								writer.println("MESSAGE " + "Nothing happened");
+							}
+						}
+						is_vote = 0;
+						count = 0;
+						for (int i = 0; i < max_client; i++)
+							vote[i] = 0;
+					}
+
+					if (kicked[mafia_index] == 0) {
+						for (PrintWriter writer : writers) {
+							writer.println("MESSAGE " + "mafia dead!, citizen win!");
+						}
+						System.exit(0);
+					} else if (current_client == 2) {
+						for (PrintWriter writer : writers) {
+							writer.println("MESSAGE " + "mafia win!");
+						}
+						System.exit(0);
+					}
 					/*****************************************************************************/
 				}
 			} catch (IOException e) { // 갑자기 catch가 나온 이유..? -> 에러 발생시
@@ -431,7 +443,6 @@ public class testServer {
 				}
 			}
 		}
-
 	}
 
 	// 어떤것을 클라이언트에게 알려주는 것인지 잘 모르겠어요~
